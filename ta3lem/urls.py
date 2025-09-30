dc910a69be2d2a816689f5d0da80a133486b148d
Muhammad Iqbal <iqbal.adudu@gmail.com>
2025-09-30 11:18:19 +0300

    add basic auth

--- ta3lem/urls.py
+++ ta3lem/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
 urlpatterns = [
+    path('accounts/login', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
